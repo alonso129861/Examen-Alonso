086876c82a5b1805b3613e5614f3d70fcbe4b76b
alonso129861 <alonsohernandezces@gmail.com>
2022-06-01 11:54:52 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -15,5 +15,24 @@ public class Main {
      public static void main(String[] args) {
     
          System.out.println("1º Gdam: Entornos de desarrollo");
+         
+         ListaProductos lp = new ListaProductos();
+         
+         Producto pro1 = new Producto("1234","semi",50,2);
+         Producto pro2 = new Producto("1235","entera",40,1);
+         Producto pro3 = new Producto("1236","semi",60,2);
+         
+         
+         lp.addProducto(pro1);
+         lp.addProducto(pro2);
+         lp.addProducto(pro3);
+         
+         pro1.imprime_detalle();
+         pro2.imprime_detalle();
+         pro3.imprime_detalle();
+         
+         
+         
+         
     }
 }
